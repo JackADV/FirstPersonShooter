c09835c0b9dd6b56871b9454081a36bfd7b2cd45
Jack <47583484+JackADV@users.noreply.github.com>
2019-05-15 15:59:17 +1000

    Adding fire mechanic

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    public Weapon currentWeapon;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If there is a current weapon and can attack
+        if (currentWeapon && currentWeapon.canAttack)
+        {
+            // if the fire button is pressed
+            if (Input.GetButton("Fire1"))
+            {
+                // Attack
+                currentWeapon.Attack();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/Projectiles/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Projectile
+{
+    public float speed = 10f; // Speed of the bullet
+    private Rigidbody rigid;
+    public Transform line;
+    public GameObject effectPrefab;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>(); // Calls the rigid body
+    }
+
+    void Update()
+    {
+        if(rigid.velocity.magnitude > 0f)
+        {
+            line.rotation = Quaternion.LookRotation(rigid.velocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (effectPrefab)
+        {
+            // Get the contact point
+            ContactPoint contact = collision.contacts[0];
+            // Spawn the effect - and rotate to contact normal
+            Instantiate(effectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+        }
+        // Destroy bullet
+        Destroy(gameObject);
+    }
+
+    public override void Fire(Vector3 lineOrigin, Vector3 direction)
+    {
+        rigid.AddForce(direction * speed, ForceMode.Impulse);
+        line.position = lineOrigin;
+    }
+
+    
+}
