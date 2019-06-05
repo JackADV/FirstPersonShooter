a7b5692137f64ed7c177ab71e7d91683a97bde0a
Jack <47583484+JackADV@users.noreply.github.com>
2019-06-05 15:45:13 +1000

    Update

--- Assets/Scripts/Combat/Projectiles/Bullet.cs
+++ Assets/Scripts/Combat/Projectiles/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : Projectile
 {
+    public int damage = 10;
     public float speed = 10f; // Speed of the bullet
     private Rigidbody rigid;
     public Transform line;
@@ -24,13 +25,19 @@ public class Bullet : Projectile
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (effectPrefab)
+        // if (effectPrefab)
+        // {
+        // Get the contact point
+        //     ContactPoint contact = collision.contacts[0];
+        // Spawn the effect - and rotate to contact normal
+        //Instantiate(effectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+        // }
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy)
         {
-            // Get the contact point
-            ContactPoint contact = collision.contacts[0];
-            // Spawn the effect - and rotate to contact normal
-            Instantiate(effectPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+            enemy.TakeDamage(damage);
         }
+        
         // Destroy bullet
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public Transform healthBarparent; // Refers to empty canvas element
+    public GameObject healthBarUiPrefab; // Healthbar prefab to spawn
+    public Transform healthBarPoint; // refers to transform for following the health bar
+
+    private int health = 0;
+    private Slider healthSlider;
+    private Renderer rend;
+    void Start()
+    {
+        // Spawn a new healthbar before the first frame update
+        GameObject clone = Instantiate(healthBarUiPrefab, healthBarparent);
+        // get slider component form new healthbar
+        healthSlider = clone.GetComponent<Slider>();
+        // Set mhealth to max health
+        health = maxHealth;
+        rend = GetComponent<Renderer>();
+    }
+    void OnDestroy()
+    {
+        if (healthSlider)
+        {
+            Destroy(healthSlider.gameObject); // Destroys the healthbar when the enemy is destroyed
+        }
+    }
+        
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // healthSlider.gameObject.SetActive(rend.isVisible);
+        // Vector3 screenPosition = Camera.main.WorldToScreenPoint(healthBarPoint.position);
+        // healthSlider.transform.position = screenPosition;
+
+        // or
+
+        // If the renderer (enemy) is visible
+        if (rend.isVisible)
+        {
+            healthSlider.gameObject.SetActive(true);
+            // Update position of healthbar with enemy
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(healthBarPoint.position);
+            healthSlider.transform.position = screenPosition;
+        }
+        else
+        {
+            // deactivate the healthbar
+            healthSlider.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        // Reduce health with damage
+        health -= damage;
+        healthSlider.value = (float)health / (float)maxHealth;
+        // if health reaches zero
+        if(health < 0)
+        {
+            // Destroy the gameobject
+            Destroy(gameObject);
+        }
+
+    }
+}
